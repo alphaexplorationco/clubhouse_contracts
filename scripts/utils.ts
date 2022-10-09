1fe9e8983b51c5ea8ec58158b59ab4c3a6c690b1
Akshaan Kakar <akshaan.crackers@gmail.com>
2022-10-09 00:41:49 -0400

    Add skeletal deploy script and drop utils

--- scripts/utils.ts
+++
@@ -1,9 +0,0 @@
-import * as fs from 'fs';
-
-export function saveContractAddress(contractName: string, chainId: number, address: string) {
-    const jsonString = fs.readFileSync('../deploy.json','utf8');
-    const deploy = JSON.parse(jsonString);
-    deploy[chainId][contractName] = address
-    fs.writeFileSync('./newCustomer.json', JSON.stringify(deploy))
-  }
-  
