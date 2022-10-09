20dbd5c0944da04129cf5452d6c8ed5217272005
Akshaan Kakar <akshaan.crackers@gmail.com>
2022-10-09 03:19:52 -0400

    Clean up deps

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,7 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@openzeppelin/hardhat-upgrades";
+import "hardhat-deploy";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
