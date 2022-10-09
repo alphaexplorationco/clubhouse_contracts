68f43b1f746e44090cc0ead075de022a870eb7ba
Akshaan Kakar <akshaan.crackers@gmail.com>
2022-10-09 03:24:01 -0400

    Add hardhat deploy plugin

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,6 +2,7 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@openzeppelin/hardhat-upgrades";
 import "hardhat-deploy";
+import "@nomiclabs/hardhat-ethers";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
