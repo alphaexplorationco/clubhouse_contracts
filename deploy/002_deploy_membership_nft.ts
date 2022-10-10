204c17ab013095484b104efcac3835d1b1220153
Akshaan Kakar <akshaan.crackers@gmail.com>
2022-10-10 02:43:25 -0400

    Clean up scripts

---
+++ deploy/002_deploy_membership_nft.ts
@@ -0,0 +1,9 @@
+import {HardhatRuntimeEnvironment} from 'hardhat/types';
+import { DeployFunction } from 'hardhat-deploy/types';
+import { deployContract } from '../src/hardhatDeployUtils';
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const membershipContractName = "MembershipERC721" 
+  await deployContract(forwarderContractName, hre)
+};
+export default func;
