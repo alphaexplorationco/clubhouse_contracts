4c8f954cfe96b3316d6056d524648b95df89f778
Akshaan Kakar <akshaan.crackers@gmail.com>
2022-11-09 22:51:42 -0500

    Use GSN forwarder

--- deploy/001_deploy_forwarder.ts
+++ deploy/001_deploy_forwarder.ts
@@ -1,11 +1,11 @@
 import {HardhatRuntimeEnvironment} from 'hardhat/types';
 import { DeployFunction } from 'hardhat-deploy/types';
-import { deployTransparentUpgradeableContract } from '../src/hardhatDeployUtils';
+import { deployContract } from '../src/hardhatDeployUtils';
 import { upgrades } from 'hardhat';
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const forwarderContractName = "Forwarder" 
-  await deployTransparentUpgradeableContract(hre, forwarderContractName, "initialize")
+  await deployContract(hre, forwarderContractName)
 };
 export default func;
 func.tags = ['hardhat', 'goerli', 'forwarder'];
