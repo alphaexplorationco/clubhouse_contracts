bcddffcfb4f8e4928c6a25bee258d6f91dbf8fa4
Akshaan Kakar <akshaan.crackers@gmail.com>
2022-11-15 13:49:33 -0500

    Add comments

--- contracts/MembershipERC721Factory.sol
+++ contracts/MembershipERC721Factory.sol
@@ -18,13 +18,24 @@ contract MembershipERC721Factory is Ownable {
 
     UpgradeableBeacon private immutable beacon;
 
+    // Mapping from proxy address to bool. Used to prove that a proxy was 
+    // created by this factory.
     mapping(address => bool) private proxyRegistry;
 
+<<<<<<< Updated upstream
     constructor(address _initBlueprint) {
         beacon = new UpgradeableBeacon(_initBlueprint);
         beacon.transferOwnership(tx.origin);
+=======
+    
+    constructor(address _initBlueprint, address beaconOwner) {
+        UpgradeableBeacon beacon = new UpgradeableBeacon(_initBlueprint);
+        beacon.transferOwnership(beaconOwner);
+        beaconAddress = address(beacon);
+>>>>>>> Stashed changes
     }
 
+    /// @notice Builds a new membership proxy contract. Can be called by contract owner only.
     function buildMembershipERC721Proxy(
         string memory _name,
         string memory _symbol,
@@ -46,14 +57,17 @@ contract MembershipERC721Factory is Ownable {
         emit MembershipERC721ProxyCreated(proxyAddress, _name, _symbol);
     }
 
+    /// @notice Returns the address for the upgradeable beacon for this factory
     function getBeacon() public view returns (address) {
         return address(beacon);
     }
 
+    /// @notice Returns the address for the underlying implementation contract for this factory
     function getImplementation() public view returns (address) {
         return beacon.implementation();
     }
 
+    /// @notice Returns true if a proxy was created by this factory, false otherwise
     function proxyWasCreatedByFactory(address proxyAddress)
         public
         view
@@ -62,6 +76,7 @@ contract MembershipERC721Factory is Ownable {
         return proxyRegistry[proxyAddress];
     }
 
+    /// @notice Overrides the renounceOwnership function from Ownable. Always reverts.
     function renounceOwnership() public view override(Ownable) onlyOwner {
         revert("Cannot renounce ownership");
     }
