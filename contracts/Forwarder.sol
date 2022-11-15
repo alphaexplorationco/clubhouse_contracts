9d69fb034dd8423fbef7026e26b3afe0dc236ff0
Akshaan Kakar <akshaan.crackers@gmail.com>
2022-11-15 13:38:05 -0500

    QSP-2 Ether Sent to Forwarder May Get Stuck in the Contract (#12)
    * QSP-2 Ether Sent to Forwarder May Get Stuck in the Contract
    
    * Update deploy addresses

--- contracts/Forwarder.sol
+++ contracts/Forwarder.sol
@@ -1,10 +1,4 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.17;
 
-import "@openzeppelin/contracts-upgradeable/metatx/MinimalForwarderUpgradeable.sol";
-
-contract Forwarder is MinimalForwarderUpgradeable {
-    function initialize() public initializer {
-        __MinimalForwarder_init();
-    }
-}
+import "@opengsn/contracts/src/forwarder/Forwarder.sol";
