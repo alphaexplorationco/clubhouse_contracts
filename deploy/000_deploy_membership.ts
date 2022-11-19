75c87c2b7f4f0b8a6562ed6deb576371f68d090e
Akshaan Kakar <akshaan.crackers@gmail.com>
2022-11-19 04:55:59 -0500

    Update deploy scripts

--- deploy/000_deploy_membership.ts
+++ deploy/000_deploy_membership.ts
@@ -1,16 +1,18 @@
 import {HardhatRuntimeEnvironment} from 'hardhat/types';
 import { DeployFunction } from 'hardhat-deploy/types';
 import { deployContract } from '../src/hardhatDeployUtils';
+import { ethers } from 'hardhat';
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 
   // Deploy implementation
-  const membershipImplementationName = "MembershipERC721" 
+  const membershipImplementationName = "MembershipERC721"
   const membershipImplemenationAddress = await deployContract(hre, membershipImplementationName)
 
   // Deploy factory
   const membershipFactoryName = "MembershipERC721Factory"
-  await deployContract(hre, membershipFactoryName, membershipImplemenationAddress)
+  const [beaconOwner] = await ethers.getSigners() 
+  await deployContract(hre, membershipFactoryName, membershipImplemenationAddress, beaconOwner.address)
 };
 export default func;
 func.tags = ['hardhat', 'goerli', 'membership'];
--- deploy/001_deploy_forwarder.ts
+++ deploy/001_deploy_forwarder.ts
@@ -1,10 +1,19 @@
 import {HardhatRuntimeEnvironment} from 'hardhat/types';
 import { DeployFunction } from 'hardhat-deploy/types';
 import { deployContract } from '../src/hardhatDeployUtils';
+import { ethers } from 'hardhat';
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const forwarderContractName = "Forwarder" 
-  await deployContract(hre, forwarderContractName)
+  const contractAddress = await deployContract(hre, forwarderContractName)
+  
+  // Register domain separator
+  const ContractFactory = await ethers.getContractFactory("Forwarder")
+  const contract = ContractFactory.attach(contractAddress)
+  const txReceipt = await (await contract.functions.registerDomainSeparator("ClubhouseForwarder", "1.0.0")).wait()
+  const domainHash = await txReceipt.events.slice(-1)[0].args.domainSeparator
+  const domainValue = await txReceipt.events.slice(-1)[0].args.domainValue
+  console.log(`✔ Registered domain separator for Forwarder with domainHash = ${domainHash}, domainValue= ${domainValue}`)
 };
 export default func;
 func.tags = ['hardhat', 'goerli', 'forwarder'];
