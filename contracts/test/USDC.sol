690cd78b8fc487b8ac44ac89ea933bee781a7857
Akshaan Kakar <akshaan.crackers@gmail.com>
2022-11-25 04:22:19 +0530

    Make test USDC contract mintable

--- contracts/test/USDC.sol
+++ contracts/test/USDC.sol
@@ -2,9 +2,14 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract USDC is ERC20 {
+contract USDC is ERC20, Ownable {
     constructor() ERC20("USDC", "USDC") {}
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
 
     function decimals() public view virtual override returns (uint8) {
         return 6;
